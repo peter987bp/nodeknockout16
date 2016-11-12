f73de5f9a4c49566b18518bb00e2472ba929ba32
SpaceToastCoatToCoast <alloalloalan@gmail.com>
2016-11-11 20:01:40 -1000

    adds comments, structures out complete animation logic

--- public/js/app.js
+++ public/js/app.js
@@ -6,15 +6,27 @@ angular.module("webPet", [])
   'PET_BASE_FRAME2',
   ($scope, $interval, PET_BASE_FRAME1, PET_BASE_FRAME2) => {
 
+    //Initial pet sprite when game is started
     $scope.petState = null;
 
     $scope.baseFrames = [PET_BASE_FRAME1, PET_BASE_FRAME2];
+    //Fill in these arrays as above
+    $scope.eggFrames = [];
+    $scope.sleepFrames = [];
+    $scope.playFrames = [];
+    $scope.eatFrames = [];
+    $scope.poopFrames = [];
+    $scope.dieFrames = [];
+
     $scope.animCount = 0;
     this.animate = $interval(() => {
+      //iterate through animation frames in a continuous loop
+      //change frame array as state changes in parent
       $scope.petState = $scope.baseFrames[++$scope.animCount % 2];
     }, (50 * $scope.health));
 
     $scope.die = () => {
+      //display death frames, then stop animation entirely
       cancel(this.animate);
     }
 
