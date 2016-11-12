fb5f5cb7224fb5deac8025ead2bc38f2ba9daa90
Renee <rchar20@gmail.com>
2016-11-12 11:41:14 -1000

    creates PoopService
    - will update pooped status to true every minute
    - ability to clean poop
    - decrements happiness by one each second that there is a poop
    
    resolves #16

--- public/js/TimeController.js
+++ public/js/TimeController.js
@@ -5,17 +5,20 @@ angular.module("webPet")
   'HappinessService',
   'HealthService',
   'HungerService',
+  'PoopService',
   class TimeController {
-    constructor($scope, EnergyService, HappinessService, HealthService, HungerService) {
+    constructor($scope, EnergyService, HappinessService, HealthService, HungerService, PoopService) {
       $scope.EnergyService = EnergyService;
       $scope.HappinessService = HappinessService;
       $scope.HealthService = HealthService;
       $scope.HungerService = HungerService;
+      $scope.PoopService = PoopService;
 
       $scope.EnergyService.init($scope);
       $scope.HappinessService.init($scope);
       $scope.HealthService.init($scope);
       $scope.HungerService.init($scope);
+      $scope.PoopService.init($scope);
 
       $scope.timer = 0;
       this.countUp;
