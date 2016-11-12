38e320d0d70f9d3c2fc8865dc5a7f8c61e45825b
peter987bp <peter987bp@gmail.com>
2016-11-12 10:49:58 -1000

    Updated happiness service

--- public/js/HappinessService.js
+++ public/js/HappinessService.js
@@ -1,30 +1,30 @@
 angular.module("webPet")
   .service('HappinessService', [
     'HealthService',
-    function(HealthService) {
+    function(HappinessService) {
     const maxHappiness = 10;
     const happinessLvl = 5;
 
+      //When happiness level is high positive effect to health
+      //When happiness level is low it it negative effect to health
+
     this.happinessLvl = happinessLvl;
-    this.getHappinessLvl = () => {
-      return this.happinessLvl;
-    }
+
     this.incrementHappinessLvl = (value) => {
       this.happinessLvl += value;
+      if(this.happinessLvl >= 10) {
+        HealthService.incrementHealth(1);
+      }
+      //increases in energy ups happiness
       return this.happinessLvl;
-    }
+    };
     this.decrementHappinessLvl = (value) => {
       this.happinessLvl -= value;
-      return this.happinessLvl;
-    }
-
-    this.reduceHappiness = () => {
-      if(this.happinessLvl - 1 <= 0) {
-        this.happinessLvl = 0
-        HealthService.reduceHealth();
-      } else {
-        --this.happinessLvl;
+      //When happiness reduces it affects health
+      if(this.happinessLvl <= ) {
+        HealthService.reduceHealth(-1);
       }
-    }
+      return this.happinessLvl;
+    };
   }])
 
