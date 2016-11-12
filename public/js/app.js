fc56c4e43dbf447becfabf1a06ce9a2a6197bbe7
peter987bp <peter987bp@gmail.com>
2016-11-12 10:03:41 -1000

    added graphics

--- public/js/app.js
+++ public/js/app.js
@@ -2,7 +2,11 @@ angular.module("webPet", [])
 .controller("petController", [
   '$scope',
   'PET_BASE_FRAME1',
-  ($scope, PET_BASE_FRAME1) => {
+  'EGG_BASE_FRAME1',
+  'POOP_BASE_FRAME1',
+  ($scope, PET_BASE_FRAME1, EGG_BASE_FRAME1, POOP_BASE_FRAME1) => {
     $scope.petState = PET_BASE_FRAME1;
+    $scope.petState = EGG_BASE_FRAME1;
+    $scope.petState = POOP_BASE_FRAME1;
   }
 ])
