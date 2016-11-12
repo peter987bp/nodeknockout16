b242d72f990753bfe3d3099ee28bd6bb4bd4760f
SpaceToastCoatToCoast <alloalloalan@gmail.com>
2016-11-11 19:49:13 -1000

    pet animates slower as health decreases

--- public/js/app.js
+++ public/js/app.js
@@ -5,13 +5,14 @@ angular.module("webPet", [])
   'PET_BASE_FRAME1',
   'PET_BASE_FRAME2',
   ($scope, $interval, PET_BASE_FRAME1, PET_BASE_FRAME2) => {
+
     $scope.petState = null;
 
     $scope.baseFrames = [PET_BASE_FRAME1, PET_BASE_FRAME2];
     $scope.animCount = 0;
     this.animate = $interval(() => {
       $scope.petState = $scope.baseFrames[++$scope.animCount % 2];
-    }, 500);
+    }, (50 * $scope.health));
 
     $scope.die = () => {
       cancel(this.animate);
