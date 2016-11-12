22481d6bb436f56b418bbcb7b80b5126d98b17a7
SpaceToastCoatToCoast <alloalloalan@gmail.com>
2016-11-11 19:34:06 -1000

    adds animation

--- public/js/app.js
+++ public/js/app.js
@@ -1,8 +1,21 @@
 angular.module("webPet", [])
-.controller("petController", [
+.controller("animController", [
   '$scope',
+  '$interval',
   'PET_BASE_FRAME1',
-  ($scope, PET_BASE_FRAME1) => {
-    $scope.petState = PET_BASE_FRAME1;
+  'PET_BASE_FRAME2',
+  ($scope, $interval, PET_BASE_FRAME1, PET_BASE_FRAME2) => {
+    $scope.petState = null;
+
+    $scope.baseFrames = [PET_BASE_FRAME1, PET_BASE_FRAME2];
+    $scope.animCount = 0;
+    this.animate = $interval(() => {
+      $scope.petState = $scope.baseFrames[++$scope.animCount % 2];
+    }, 500);
+
+    $scope.die = () => {
+      cancel(this.animate);
+    }
+
   }
 ])
