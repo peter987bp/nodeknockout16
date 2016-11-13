95e9bbb6a975508d1860e0d5573cbc5e9f62ea79
SpaceToastCoatToCoast <alloalloalan@gmail.com>
2016-11-12 14:45:00 -1000

    represents poop with a graphic rather than a boolean

--- public/js/app.js
+++ public/js/app.js
@@ -10,6 +10,7 @@ angular.module("webPet", [])
 
     //Initial pet sprite when game is started
     $scope.petState = null;
+    $scope.poopState = "";
 
     $scope.baseFrames = [PET_BASE_FRAME1, PET_BASE_FRAME2];
     //Fill in these arrays as above
@@ -24,6 +25,11 @@ angular.module("webPet", [])
       //iterate through animation frames in a continuous loop
       //change frame array as state changes in parent
       $scope.petState = $scope.baseFrames[$scope.timer % 2];
+      if($scope.pooped) {
+        $scope.poopState = $scope.poopFrames[0];
+      } else if(!$scope.pooped) {
+        $scope.poopState = "";
+      }
     }, (50 * (20 - $scope.health)));
 
     $scope.die = () => {
