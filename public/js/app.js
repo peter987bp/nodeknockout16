f66ec479a704a25612096b84493ccaaec36a5960
SpaceToastCoatToCoast <alloalloalan@gmail.com>
2016-11-12 15:43:07 -1000

    fixes animation state controller

--- public/js/app.js
+++ public/js/app.js
@@ -22,9 +22,32 @@ angular.module("webPet", [])
   'PLAY_BASE_FRAME1',
   'PLAY_BASE_FRAME2',
   'DEATH_BASE_FRAME1',
-  ($scope, $interval, PET_BASE_FRAME1, PET_BASE_FRAME2, EGG_BASE_FRAME1, EGGHATCH_BASE_FRAME1, EGGHATCH_BASE_FRAME2, POOP_BASE_FRAME1,POOP_BASE_FRAME2,POOP_BASE_FRAME3, POOP_BASE_FRAME4,POOP_BASE_FRAME5, FOOD_BASE_FRAME1, FOOD_BASE_FRAME2, FOOD_BASE_FRAME3, FOOD_BASE_FRAME4,SLEEP_BASE_FRAME1,SLEEP_BASE_FRAME2,SLEEP_BASE_FRAME3,PLAY_BASE_FRAME1,PLAY_BASE_FRAME1,DEATH_BASE_FRAME1) => {
+  (
+    $scope,
+    $interval,
+    PET_BASE_FRAME1,
+    PET_BASE_FRAME2,
+    EGG_BASE_FRAME1,
+    EGGHATCH_BASE_FRAME1,
+    EGGHATCH_BASE_FRAME2,
+    EGGHATCH_BASE_FRAME3,
+    EGGHATCH_BASE_FRAME4,
+    EGGHATCH_BASE_FRAME5,
+    POOP_BASE_FRAME1,
+    POOP_BASE_FRAME2,
+    FOOD_BASE_FRAME1,
+    FOOD_BASE_FRAME2,
+    FOOD_BASE_FRAME3,
+    FOOD_BASE_FRAME4,
+    SLEEP_BASE_FRAME1,
+    SLEEP_BASE_FRAME2,
+    SLEEP_BASE_FRAME3,
+    PLAY_BASE_FRAME1,
+    PLAY_BASE_FRAME2,
+    DEATH_BASE_FRAME1
+  ) => {
     //Initial pet sprite when game is started
-    $scope.petState = null;
+    $scope.petState = EGG_BASE_FRAME1;
     $scope.poopState = "";
 
     $scope.baseFrames = [PET_BASE_FRAME1, PET_BASE_FRAME2];
@@ -40,11 +63,20 @@ angular.module("webPet", [])
     this.animate = $interval(() => {
       //iterate through animation frames in a continuous loop
       //change frame array as state changes in parent
-      $scope.petState = $scope.baseFrames[$scope.timer % 2];
-      if($scope.pooped) {
-        $scope.poopState = $scope.poopFrames[0];
-      } else if(!$scope.pooped) {
-        $scope.poopState = "";
+      if($scope.timer < 8) {
+        //hatch during the first few frames
+        $scope.petState = $scope.eggHatchFrames[$scope.timer % $scope.eggHatchFrames.length];
+      } else {
+        //main state
+        //base state -  nothing happening
+        $scope.petState = $scope.baseFrames[$scope.timer % 2];
+
+        //check if pooped, asleep, or playing and change animation accordingly
+        if($scope.pooped) {
+          $scope.poopState = $scope.poopFrames[$scope.timer % 2];
+        } else if(!$scope.pooped) {
+          $scope.poopState = "";
+        }
       }
     }, (50 * (20 - $scope.health)));
 
